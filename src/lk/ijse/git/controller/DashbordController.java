d1210eb92caed3a10249ae18958592f29c90c407
nipunChathura <nipunc1999@gmail.com>
2021-08-07 12:18:07 +0530

    Create dashbord

--- src/lk/ijse/git/controller/DashbordController.java
+++ src/lk/ijse/git/controller/DashbordController.java
@@ -12,8 +12,16 @@
 
 package lk.ijse.git.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,8 +30,31 @@ import java.util.ResourceBundle;
  * @since : 8/7/2021
  **/
 public class DashbordController implements Initializable {
+
+    @FXML
+    private JFXButton btnCustomer;
+
+    @FXML
+    private JFXButton btnItem;
+
+    private Stage stage = new Stage();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void customer(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(CustomerController.class.getResource("/lk/ijse/git/view/CustomerForm.fxml"));
+        Scene temp = new Scene(root);
+        stage.setScene(temp);
+        stage.show();
+    }
+
+    public void item(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(ItemController.class.getResource("/lk/ijse/git/view/ItemForm.fxml"));
+        Scene temp = new Scene(root);
+        stage.setScene(temp);
+        stage.show();
     }
 }
